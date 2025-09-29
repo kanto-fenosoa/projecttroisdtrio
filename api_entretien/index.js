a3dac7ce45972105805350431e9f16f83472d1f2
Mirado1679 <rutamickael619@gmail.com>
2025-09-29 14:19:54 +0300

    Mis en place du serveur

---
+++ api_entretien/index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('Manjaaaaakkeeeellllyyyy ahhhhh 🤣🤣🤣')
+}
+)
+app.listen(3000,()=>console.log("mandeha le api"))
